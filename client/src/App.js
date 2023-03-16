80eff25e97b664e67f61e558e077a53713dd30b2
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-15 22:36:24 -0300

    feat: Roteamento inicial do app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
 
-function App() {
-  return (
-      <div>
-          <p>Hello World!</p>
-      </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/resume' element={<Resume />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
