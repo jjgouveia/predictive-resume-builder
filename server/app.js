0f159de2fd3481ea63751420d75873dea2d7556a
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-15 23:52:58 -0300

    feat: Configurando api da OPEN IA

--- server/app.js
+++ server/app.js
@@ -1,16 +1,35 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
-
-
-
 const app = express();
+const { Configuration, OpenAIApi } = require("openai");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_TOKEN,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const GPTFunction = async (text) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        temperature: 0.6,
+        max_tokens: 250,
+        top_p: 1,
+        frequency_penalty: 1,
+        presence_penalty: 1,
+    });
+    return response.data.choices[0].text;
+};
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
