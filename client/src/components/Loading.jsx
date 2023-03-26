1cf750b8e7ca722d3a5b5ff528b6d7de596b7634
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-26 00:57:57 -0300

    refactor: MAis correções de sintaxe

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const Loading = () => {
-    return (
-        <div className='app'>
-            <h1>Carregando. Por favor, aguarde...</h1>
-        </div>
-    );
-};
+function Loading() {
+  return (
+    <div className="app">
+      <h1>Carregando. Por favor, aguarde...</h1>
+    </div>
+  );
+}
 
 export default Loading;
--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,91 +1,106 @@
-import React, { useRef } from "react";
-import ErrorPage from "./ErrorPage";
-import { useReactToPrint } from "react-to-print";
-import { Link } from "react-router-dom";
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import ErrorPage from './ErrorPage';
 
-const Resume = ({ result }) => {
+function Resume({ result }) {
+  const componentRef = useRef();
 
-    const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: `${result.fullName} Currículo`,
+    onAfterPrint: () => alert('Print Successful!'),
+  });
 
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-        documentTitle: `${result.fullName} Currículo`,
-        onAfterPrint: () => alert("Print Successful!"),
-    });
+  const replaceWithBr = (string) => string.replace(/\n/g, '<br />');
 
-    const replaceWithBr = (string) => {
-        return string.replace(/\n/g, "<br />");
-    };
+  if (JSON.stringify(result) === '{}') {
+    return <ErrorPage />;
+  }
 
-    if (JSON.stringify(result) === "{}") {
-        return <ErrorPage />;
-    }
+  return (
+    <>
+      <div className="buttonGroup">
+        <button type="button" onClick={handlePrint}>Imprimir</button>
+        <Link to="/send/resume" className="sendEmail">
+          Enviar por email
+        </Link>
+      </div>
+      <main className="container" ref={componentRef}>
+        <header className="header">
+          <div>
+            <h1>{result.fullName}</h1>
+            <p className="resumeTitle headerTitle">
+              {result.currentPosition}
+              {' '}
+              (
+              {result.currentTechnologies}
+              )
+            </p>
+            <p className="resumeTitle">
+              {result.currentLength}
+              {' '}
+              ano(s) de experiência
+            </p>
+          </div>
+          <div>
+            <img
+              src={result.image_url}
+              alt={result.fullName}
+              className="resumeImage"
+            />
+          </div>
+        </header>
+        <div className="resumeBody">
+          <div>
+            <h2 className="resumeBodyTitle">RESUMO DO PERFIL</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.objective),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">HISTÓRICO DE TRABALHO</h2>
+            {result.workHistory.map((work) => (
+              <p className="resumeBodyContent" key={work.name}>
+                <span style={{ fontWeight: 'bold' }}>{work.name}</span>
+                {' '}
+                -
+                {' '}
+                {work.position}
+              </p>
+            ))}
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">PERFIL DE TRABALHO</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.jobResponsibilities),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">RESPONSABILIDADES</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.keypoints),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
 
-
-    return (
-        <>
-        <div className='buttonGroup'>
-				<button onClick={handlePrint}>Imprimir</button>
-				<Link to='/send/resume' className='sendEmail'>
-					Enviar por email
-				</Link>
-			</div>
-        <main className='container' ref={componentRef}>
-            <header className='header'>
-                <div>
-                    <h1>{result.fullName}</h1>
-                    <p className='resumeTitle headerTitle'>
-                        {result.currentPosition} ({result.currentTechnologies})
-                    </p>
-                    <p className='resumeTitle'>
-                        {result.currentLength} ano(s) de experiência
-                    </p>
-                </div>
-                <div>
-                    <img
-                        src={result.image_url}
-                        alt={result.fullName}
-                        className='resumeImage' />
-                </div>
-            </header>
-            <div className='resumeBody'>
-                <div>
-                    <h2 className='resumeBodyTitle'>RESUMO DO PERFIL</h2>
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: replaceWithBr(result.objective),
-                        }}
-                        className='resumeBodyContent' />
-                </div>
-                <div>
-                    <h2 className='resumeBodyTitle'>HISTÓRICO DE TRABALHO</h2>
-                    {result.workHistory.map((work) => (
-                        <p className='resumeBodyContent' key={work.name}>
-                            <span style={{ fontWeight: "bold" }}>{work.name}</span> -{" "}
-                            {work.position}
-                        </p>
-                    ))}
-                </div>
-                <div>
-                    <h2 className='resumeBodyTitle'>PERFIL DE TRABALHO</h2>
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: replaceWithBr(result.jobResponsibilities),
-                        }}
-                        className='resumeBodyContent' />
-                </div>
-                <div>
-                    <h2 className='resumeBodyTitle'>RESPONSABILIDADES</h2>
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: replaceWithBr(result.keypoints),
-                        }}
-                        className='resumeBodyContent' />
-                </div>
-            </div>
-        </main>
-        </>
-    );
-};
+Resume.propTypes = {
+  result: PropTypes.objectOf({}),
+}.isRequired;
 
 export default Resume;
